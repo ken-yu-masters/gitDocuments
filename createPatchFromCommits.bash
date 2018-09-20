f55eda764d25e11fea83b409573fecb5085f8a4d
ken-yu-masters <platinicslug@gmail.com>
2018-09-21 09:54:49 +1200

    Update createPatchFromCommits.bash

--- createPatchFromCommits.bash
+++ createPatchFromCommits.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
+#will create patch files for following commits.
 commits="d470695c4a7 f51f3ff9088 9757992351e fd5cd2c3e9f 17d2edbd0e5 6bc9ba8a96e 873fa9f32eb 9339362222f 7d7b6024805 c796ff6e823 8be836c4dea fe520940404"
+#patch file will be outputed here.
 outputDir='S:\patches'
+
 count=0
 for co in $commits ; do
+	#git format-patch generate file names with ugly names, need convert to more readable.
 	fileName=`git format-patch $co -1 -o $outputDir`
 	newFileName=`echo $fileName | awk -F "-" '{for (i=2;i<NF;i++)printf "%s_", $i;printf "%s",$NF}'`
 	newFileName="${outputDir}\\${count}-${co}-${newFileName}"
